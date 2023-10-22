569b2d0f841294ca3f7b03d355db8ab0f535653b
sagred <sagred17@gmail.com>
2023-10-22 19:26:06 -0400

    add coinbase service

---
+++ serverless/services/coinbase.js
@@ -0,0 +1,7 @@
+import axios from 'axios';
+import crypto from 'crypto';
+
+export const getCoinbaseBalance = (oauthToken) => {
+    // Call Coinbase API using the OAuth token to get the balance
+    // Return the balance
+}
